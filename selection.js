7fecee9c168e41a5f8b0d57546fa88bf7a06692b
iigmir <roc120j@gmail.com>
2016-05-12 22:54:58 +0800

    add step var in selection.js

--- selection.js
+++ selection.js
@@ -1,6 +1,8 @@
 // Selection Sort
 var ExchangeInput =[69,81,30,38,9,2,47,61,32,79];
 var selectionInput = ExchangeInput;
+var exchangeStep = 0;
+var selectionStep = 0;
 // Cause it like Exchange sort, so firstly start from Exchange sort
 for (var x=0; x<ExchangeInput.length;x++)
 {
@@ -12,6 +14,7 @@ for (var x=0; x<ExchangeInput.length;x++)
             ExchangeInput[x] = ExchangeInput[y];
             ExchangeInput[y] = ExchangeAnchor;
         }
+        exchangeStep++;
     }
 }
 
@@ -32,4 +35,8 @@ for (var x=0; x<selectionInput.length;x++)
         selectionInput[x] = selectionInput[selectionVal];
         selectionInput[selectionVal] = selectionChangeVal;
     }
+    selectionStep++;
 }
+
+// I add step var to watch how many step should loop did. Result is 55 versus 10.
+// I am scared.
