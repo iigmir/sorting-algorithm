093b2507a30f52cd693895f17ba030e8b0c19a14
iigmir <roc120j@gmail.com>
2016-05-13 11:51:45 +0800

    Add second also final step

--- insertion.js
+++ insertion.js
@@ -5,4 +5,16 @@ var insertOutput = [];
 insertOutput.push(insertInput[0]);
 if ( insertInput[0] < insertInput[1] ) { insertOutput.push(insertInput[1]); }
 else { insertOutput.unshift(insertInput[1]); }
-console.log(insertOutput);
+// Second, start from third element
+// Ref: http://stackoverflow.com/questions/586182/how-to-insert-an-item-into-an-array-at-a-specific-index
+for (var i=2; i<insertInput.length; i++)
+{
+    for (var j=0; j<insertOutput.length; j++)
+    {
+        if( insertInput[i] < insertOutput[j] )
+        {
+            insertOutput.splice( j, 0, insertInput[i] );
+            break;
+        }
+    }
+}
